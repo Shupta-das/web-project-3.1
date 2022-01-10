5cc8a08e3b71e51d2b4fbb163020471570cd90ed
Shupta-das <shuptadas25203@gmail.com>
2022-01-10 22:37:09 +0600

    manager home page updated

--- manager/managerHome.php
+++ manager/managerHome.php
@@ -1,3 +1,6 @@
+<?php 
+    include "connection.php";
+?>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,9 +10,35 @@
     <link rel="stylesheet" href="styleManager.css">
 </head>
 <body>
-    <div class="manager-home-page">
+    <h1 class="manager-header">Manager Section ~</h1>
+    <div class="manager-home-container">
+        <div class="manager-home-left">
+            <h4>Notifications-</h4>
+            <?php 
+                $query="SELECT * FROM foodorder ORDER BY orderDate ASC LIMIT 5;";
+                $result=mysqli_query($connection,$query);
+                if(!$result){
+                    dic("query failed" . mysqli_error());
+                }
+                while($row=mysqli_fetch_assoc($result)){
+                    $userId=$row['userId'];
+                    $fId=$row['fId'];
+                    $amount=$row['amount'];
+                    echo ">>>User Id : ". $userId . " ordered for food no. : " . $fId . " amount : " . $amount . "<br>";
+                }
+
+            ?>
+
+        </div>
+        <div class="manager-home-right">
+            <h4>Menu-</h4>
+            <button class="manager-button"><a href="foodTable.php">Food</a></button><br><br>
+            <button class="manager-button"><a href="foodCategory.php">Food Category</a> </button><br><br>
+            <button class="manager-button"><a href="foodOrder.php">Food Orders</a></button>
+
+        </div>
         
     </div>
      
 </body>
-</html>
+</html> 
